0917944c2ecdf63157daddb8cc624bb17978404b
KhoroshenkoDE <dima2121988@gmail.com>
2019-05-22 22:48:22 +0300

    Create index.js
    создали index.HTML и text,txt

---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var file = fs.readFileSync('text.txt', 'utf-8');
+console.log(file);
+
+var mess = file + '\n' + 'Hello ';
+var mess1 = mess + '\n' + 'Honda';
+
+fs.writeFileSync('new_file.txt', mess );
+fs.writeFileSync('new_file.txt', mess1 );
+
+fs.readFile('new_file.txt', 'utf-8', function(err, data) {
+	console.log(data)
+});
+
+fs.writeFile('new.txt', mess, function(err, data) {});
+
+fs.appendFileSync("text.txt", "Привет ми ми ми!");
+ 
+fs.appendFile("text.txt", "Привет МИД!", 'utf-8', function(error){
+
+    var data = fs.readFileSync("text.txt", "utf8");
+    console.log(data); 
+});
+
+console.log('Test');
